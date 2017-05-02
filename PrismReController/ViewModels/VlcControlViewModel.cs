38e16028a904a769e558042802a698bd4b438d53
あると <mail@twinkfrag.net>
2017-05-02 18:34:25 +0900

    WhenAllに渡すのはIE<Task>になっていればよく，WhenAllをawaitしてるためUIにも影響しない

--- PrismReController/ViewModels/VlcControlViewModel.cs
+++ PrismReController/ViewModels/VlcControlViewModel.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Livet;
 using Livet.Commands;
 using PrismReController.Models;
 using PrismReController.Shared.Utils;
-using VlcCtrl;
 
 namespace PrismReController.ViewModels
 {
@@ -16,13 +12,13 @@ namespace PrismReController.ViewModels
 		public VlcControlViewModel()
 		{
 			PlayCommand = new ViewModelCommand(
-				async () => await Connection.Connectings.Select(async x => await x.Vlc.Play()).WhenAll(), 
+				async () => await Connection.Connectings.Select(x => x.Vlc.Play()).WhenAll(), 
 				() => Connection.Connectings.Any(x => x.Vlc?.IsConnected ?? false));
 			PauseCommand = new ViewModelCommand(
-				async () => await Connection.Connectings.Select(async x => await x.Vlc.Pause()).WhenAll(),
+				async () => await Connection.Connectings.Select(x => x.Vlc.Pause()).WhenAll(),
 				() => Connection.Connectings.Any(x => x.Vlc?.IsConnected ?? false));
 			StopCommand = new ViewModelCommand(
-				async () => await Connection.Connectings.Select(async x => await x.Vlc.Stop()).WhenAll(),
+				async () => await Connection.Connectings.Select(x => x.Vlc.Stop()).WhenAll(),
 				() => Connection.Connectings.Any(x => x.Vlc?.IsConnected ?? false));
 
 			Connection.VlcConnectingsRaiseChanged += CommandsRaiseCanExecuteChanged;
